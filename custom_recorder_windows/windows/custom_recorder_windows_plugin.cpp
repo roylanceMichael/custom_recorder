9b09809aa3194b0e1c1d9a494b7c8a48ca74654e
Mike Roylance <roylance.michael@gmail.com>
2025-07-07 06:18:23 -0600

    "fix"

--- custom_recorder_windows/windows/custom_recorder_windows_plugin.cpp
+++ custom_recorder_windows/windows/custom_recorder_windows_plugin.cpp
@@ -1,5 +1,6 @@
 #include "custom_recorder_windows_plugin.h"
 
+// Standard C++ headers
 #include <windows.h>
 #include <mmsystem.h>
 #include <flutter/method_channel.h>
@@ -10,6 +11,32 @@
 #include <sstream>
 
 #pragma comment(lib, "winmm.lib")
+
+namespace {
+
+std::wstring Utf8ToUtf16(const std::string& utf8_string) {
+  if (utf8_string.empty()) {
+    return std::wstring();
+  }
+  int target_length =
+      ::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, utf8_string.data(),
+                            static_cast<int>(utf8_string.length()), nullptr, 0);
+  if (target_length == 0) {
+    return std::wstring();
+  }
+  std::wstring utf16_string;
+  utf16_string.resize(target_length);
+  int converted_length =
+      ::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, utf8_string.data(),
+                            static_cast<int>(utf8_string.length()),
+                            utf16_string.data(), target_length);
+  if (converted_length == 0) {
+    return std::wstring();
+  }
+  return utf16_string;
+}
+
+}  // namespace
 
 namespace custom_recorder_windows {
 
@@ -43,15 +70,16 @@ void CustomRecorderWindowsPlugin::HandleMethodCall(
     auto path_value = arguments->find(flutter::EncodableValue("path"));
     if (path_value != arguments->end()) {
         std::string path = std::get<std::string>(path_value->second);
-        mciSendString("open new type waveaudio alias recorder", NULL, 0, NULL);
-        std::string command = "record recorder " + path;
+        std::wstring wpath = Utf8ToUtf16(path);
+        mciSendString(L"open new type waveaudio alias recorder", NULL, 0, NULL);
+        std::wstring command = L"record recorder " + wpath;
         mciSendString(command.c_str(), NULL, 0, NULL);
         recording_path_ = path;
     }
     result->Success();
   } else if (method_call.method_name().compare("stopRecording") == 0) {
-    mciSendString("stop recorder", NULL, 0, NULL);
-    mciSendString("close recorder", NULL, 0, NULL);
+    mciSendString(L"stop recorder", NULL, 0, NULL);
+    mciSendString(L"close recorder", NULL, 0, NULL);
     result->Success(flutter::EncodableValue(recording_path_));
   }
   else {
