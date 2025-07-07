6f9b7420011798730c703119ba03b0cec6917b09
Mike Roylance <roylance.michael@gmail.com>
2025-07-07 07:44:14 -0600

    android producing a file at least

--- custom_recorder_android/lib/custom_recorder_android.dart
+++ custom_recorder_android/lib/custom_recorder_android.dart
@@ -1,18 +1,20 @@
 import 'package:custom_recorder_platform_interface/custom_recorder_platform_interface.dart';
+import 'package:flutter/services.dart';
 
 class CustomRecorderAndroid extends CustomRecorderPlatform {
+  final MethodChannel _channel = const MethodChannel('custom_recorder_android');
+
   static void registerWith() {
     CustomRecorderPlatform.instance = CustomRecorderAndroid();
   }
 
   @override
   Future<void> startRecording({required String path}) async {
-    // Placeholder implementation
+    return _channel.invokeMethod('startRecording', {'path': path});
   }
 
   @override
   Future<String?> stopRecording() async {
-    // Placeholder implementation
-    return null;
+    return _channel.invokeMethod('stopRecording');
   }
 }
